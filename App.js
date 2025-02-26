2e2a5004968f6c7881fb392b316a1a75715d69fa
Glenn <glennpunio101@gmail.com>
2025-02-26 15:52:42 +0800

    FLEX

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  StyleSheet,
+  Dimensions,
+  SafeAreaView,
+  Text,
+  View,
+  Platform,
+  } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={{ flexDirection: "row",
+      backgroundColor: 'white',
+      flex: 1, justifyContent: 'center',
+      alignItems: 'center',
+      alignContent: 'center',
+      flexWrap: 'wrap',  
+         }}>
+      <View style={{ backgroundColor: 'blue', width: 100, height: 100,  }} />
+      <View style={{ backgroundColor: 'tomato', width: 100, height: 100 }} />
+      <View style={{ backgroundColor: 'pink', width: 100, height: 100 }} />
+      <View style={{ backgroundColor: 'gray', width: 100, height: 100 }} />
+      <View style={{ backgroundColor: 'black', width: 100, height: 100 }} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+//   // },
+// });
