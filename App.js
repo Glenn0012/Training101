296e0ea6c41cc2f12f43dd175d65458617b918b8
Glenn <glennpunio101@gmail.com>
2025-03-01 12:53:45 +0800

    Styling

--- App.js
+++ App.js
@@ -1,32 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {
-  StyleSheet,
-  Dimensions,
-  SafeAreaView,
-  Text,
-  View,
-  Platform,
-  } from 'react-native';
-
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import Calculator from './app/screens/Calculator';
+import SecondaryScreen from './app/screens/SecondaryScreen';
+import DataScreen, {Data} from './app/screens/DataScreen';
+// import {
+//   StyleSheet,
+//   Dimensions,
+//   SafeAreaView,
+//   Text,
+//   View,
+//   Platform,
+//   } from 'react-native';
+  
 export default function App() {
-  return (
-
-      <View style={{ flexDirection: "row",
-      backgroundColor: 'white',
-      flex: 1, justifyContent: 'center',
-      alignItems: 'center',
-      alignContent: 'center',
-      flexWrap: 'wrap',  
-         }}>
-      <View style={{ backgroundColor: 'blue', width: 100, height: 100,  }} />
-      <View style={{ backgroundColor: 'tomato', width: 100, height: 100 }} />
-      <View style={{ backgroundColor: 'pink', width: 100, height: 100 }} />
-      <View style={{ backgroundColor: 'gray', width: 100, height: 100 }} />
-      <View style={{ backgroundColor: 'black', width: 100, height: 100 }} />
-    </View>
-  );
+   return <DataScreen/>;
+  //  <SecondaryScreen/>;
 }
+  //  <WelcomeScreen />
+  //  <Calculator/>
+  // //  ;
+  // return
+  //
+  //  
+  
+  
+
+    //   <View style={{ flexDirection: "row",
+    //   backgroundColor: 'white',
+    //   flex: 1, justifyContent: 'center',
+    //   alignItems: 'center',
+    //   alignContent: 'center',
+    //   // flexWrap: 'wrap',  
+    //      }}>
+    //   <View style={{ backgroundColor: 'blue', width: 600, height: 100, flexShrink:1, }} />
+    //   <View style={{ backgroundColor: 'tomato', width: 100, height: 100, top:20, right:20, position:"relative" }} />
+    //   <View style={{ backgroundColor: 'pink', width: 100, height: 100 }} />
+    //   {/* <View style={{ backgroundColor: 'gray', width: 100, height: 100 }} />
+    //   <View style={{ backgroundColor: 'black', width: 100, height: 100 }} /> */}
+    // </View>
+  // );
 
 // const styles = StyleSheet.create({
 //   container: {
